681152ee3e74607986ef6bc529f9ecf6a7111037
Clivern <hello@clivern.com>
2021-03-20 21:48:00 +0100

    update

---
+++ bin/transfer.sh
@@ -0,0 +1,3 @@
+rm -rf $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/Snippets
+mkdir $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/Snippets
+cp -r "$(pwd)" $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/Snippets
